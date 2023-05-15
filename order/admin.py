33e144c041e9c1686b2ef0267613ef6c9707e9ec
Odina <md.dilyaramustafaeva@gmail.com>
2023-05-15 15:35:47 +0500

    orders , cart , payment , category test

--- order/admin.py
+++ order/admin.py
@@ -1,19 +1,19 @@
 from django.contrib import admin
 
-from order.models import order, cart, payment
+from order.models import Order, Payment, Cart
 
 
 # Register your models here.
-@admin.register(order)
+@admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ["customer", "created_at"]
 
 
-@admin.register(cart)
+@admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
     list_display = ["user", "total_price"]
 
 
-@admin.register(payment)
+@admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
     list_display = ["cart", "price"]
--- order/models.py
+++ order/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 
-class order(models.Model):
+class Order(models.Model):
     customer = models.CharField(null=True, max_length=30)
-    prize = models.FloatField()
+    price = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -11,11 +11,11 @@ class order(models.Model):
         return f"{self.customer}"
 
 
-class cart(models.Model):
+class Cart(models.Model):
     user = models.ForeignKey("users.User", on_delete=models.CASCADE, null=True, related_name="user_order")
     product = models.ForeignKey("products.Product", on_delete=models.CASCADE, related_name="product")
     total_price = models.FloatField()
-    order = models.ForeignKey(order, on_delete=models.CASCADE, related_name="orders")
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="orders")
     quantity = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -23,8 +23,8 @@ class cart(models.Model):
         return f"{self.user}"
 
 
-class payment(models.Model):
-    cart = models.ForeignKey(cart, on_delete=models.CASCADE, related_name="payment_cart")
+class Payment(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="payment_cart")
     price = models.IntegerField()
     provider = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
